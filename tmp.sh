0f453e9c870b79c5e0869f2046760c2afae2e86a
Lasse Byrnak <lasse@byrnak.com>
2020-02-13 00:16:28 +0000

    added Grafana fix

--- tmp.sh
+++ tmp.sh
@@ -3,3 +3,7 @@
 mkdir Mounts/influxdb_data
 chown 999:999 Mounts/influxdb_data
 chmod 755 Mounts/influxdb_data
+
+mkdir Mounts/grafana_data
+chown 472:472 Mounts/grafana_data
+chmod 777 Mounts/grafana_data
